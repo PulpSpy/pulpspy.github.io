b10255fa51bacfcb94bdea6b4842e6f90f4de65e
Jeremy Clark <pulpspy@gmail.com>
2023-04-14 09:31:42 -0400

    more favicon work

--- common/include.php
+++ common/include.php
@@ -12,7 +12,9 @@ function startPage(){
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="icon" href="/images/favicon.ico?v=M44lzPylqQ">
   <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
 </head>
 
 <body>
@@ -55,6 +57,8 @@ function endPage()
 {
 ?>
 <!--Favicon: orange by Pris from <a href="https://thenounproject.com/browse/icons/term/orange/" target="_blank" title="orange Icons">Noun Project</a>-->
+
+
 </div>
 </div>
 </body>
